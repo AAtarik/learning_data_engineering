1a30957723b808f6eb451d3bc15b9a741a50fa3e
Abdullah Tarek <abdullahattarik3@gmail.com>
2026-06-22 19:28:17 +0600

    New folder

---
+++ Phase-0-Foundations/Python-Fundamentals/file_handling/file_handling.py
@@ -0,0 +1,5 @@
+file = open("file_handling\data.txt", "r")
+
+content = file.read()
+print(content)
+file.close()
---
+++ Phase-0-Foundations/Python-Fundamentals/file_handling/write.py
@@ -0,0 +1,6 @@
+file = open("file_handling\data.txt", "w")
+
+file.write("\nNew data appended")
+
+file.close()
+print("file closed")
